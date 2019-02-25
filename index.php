35671cdcde87b436df0d17290a5c6555fcd0393a
Frank Sierra <sierrafayad@gmail.com>
2019-02-25 09:23:45 -0500

    Testing File

--- index.php
+++ index.php
@@ -5,21 +5,9 @@
  * Date: 2/19/2019
  * Time: 8:22 PM
  */
-@mkdir(__DIR__ . "/sql/", 0655, true);
 $start_time = time();
-if (function_exists('mb_ereg_replace')) {
-    function mb_escape(string $string)
-    {
-        return mb_ereg_replace('[\x00\x0A\x0D\x1A\x22\x27\x5C]', '\\\0', $string);
-    }
-} else {
-    function mb_escape(string $string)
-    {
-        return preg_replace('~[\x00\x0A\x0D\x1A\x22\x27\x5C]~u', '\\\$0', $string);
-    }
-}
 
-
+@mkdir(__DIR__ . "/sql/", 0655, true);
 global $entity_handlers;
 $entity_handlers = array(
     "nodes" => null,
@@ -33,9 +21,9 @@ $entity_handlers = array(
 );
 foreach ($entity_handlers as $entity => &$handler) {
     $file_name = "sql/insert_" . $entity . ".sql";
-    if (is_file($file_name)) {
-        unlink($file_name);
-    }
+//    if (is_file($file_name)) {
+//        unlink($file_name);
+//    }
     $handler = fopen($file_name, "a+");
     if (!$handler) {
         die();
@@ -53,9 +41,12 @@ $osm = new OSMReader($handle);
 $file_header = $osm->readFileHeader();
 
 
-$index = 1046;
+$index = 0;     // DenseNodes
+//$index = 1049;  // Ways
+//$index = 1157;  // Relation
+$index = 1061;
 $osm->skipToBlock($index);
-while ($osm->next()) {
+while ($data = $osm->next()) {
     $reader = $osm->getReader();
     $current = $reader->getPosition();
     $total = $reader->getEofPosition();
@@ -63,10 +54,11 @@ while ($osm->next()) {
     $index++;
 
     $entities = $osm->getElements();
+    echo " " . $entities["type"] . "\n";
     makeEntity($entities["type"], $entities["data"]);
-
+    $entities = null;
+    echo "ok";
 }
-
 $end_time = time();
 echo "This process took " . ($end_time - $start_time) . " seconds";
 
@@ -78,14 +70,23 @@ function makeEntity($entity, $data)
             $entity_handlers[$entity . "s"],
             insert_entity($entity, $datum)
         );
-        foreach ($datum["tags"] as $tag_key => $tag_value) {
-            $tag = array(
-                "key" => $tag_key,
-                "value" => $tag_value,
-            );
+        foreach ($datum["tags"] as $tag) {
             fwrite(
                 $entity_handlers[$entity . "_tags"],
                 insert_entity_tag($entity, $datum["id"], $tag)
+            );
+        }
+
+        foreach ($datum["nodes"] as $node) {
+            fwrite(
+                $entity_handlers[$entity . "_nodes"],
+                insert_entity_node($entity, $datum["id"], $node)
+            );
+        }
+        foreach ($datum["relations"] as $relation) {
+            fwrite(
+                $entity_handlers[$entity . "_members"],
+                insert_entity_member($entity, $datum["id"], $relation)
             );
         }
 
@@ -95,27 +96,25 @@ function makeEntity($entity, $data)
 
 function insert_entity($entity, $values)
 {
-    $insert_data = array(
-        "id" => $values["id"],
-        "latitude" => $values["latitude"],
-        "longitude" => $values["longitude"],
-        "changeset_id" => $values["changeset_id"],
-        "visible" => $values["visible"],
-        "timestamp" => $values["timestamp"],
-        "version" => $values["version"],
-        "user" => $values["user"],
-    );
+    $insert_data = array();
+
+    $insert_data["id"] = $values["id"];
+    if ($entity == "node") {
+        $insert_data["latitude"] = $values["latitude"];
+        $insert_data["longitude"] = $values["longitude"];
+    }
+    $insert_data["changeset_id"] = $values["changeset_id"];
+    $insert_data["visible"] = $values["visible"];
+    $insert_data["timestamp"] = $values["timestamp"];
+    $insert_data["version"] = $values["version"];
+    $insert_data["user"] = $values["user"];
 
     if (isset($values["timestamp"])) {
         $insert_data["timestamp"] = str_replace("T", " ", $insert_data["timestamp"]);
         $insert_data["timestamp"] = str_replace("Z", "", $insert_data["timestamp"]);
     }
-    if ($entity != "node") {
-        unset($insert_data["latitude"]);
-        unset($insert_data["longitude"]);
-    }
 
-    $escaped_values = array_map('mb_escape', array_values($insert_data));
+    $escaped_values = array_map('escape', array_values($insert_data));
     $values = "'" . implode("', '", $escaped_values) . "'";
     return "INSERT INTO " . $entity . "s VALUES ($values);\n";
 }
@@ -128,38 +127,42 @@ function insert_entity_tag($entity, $id, $values)
         "v" => $values["value"]
     );
 
-    $escaped_values = array_map('mb_escape', array_values($insert_data));
+    $escaped_values = array_map('escape', array_values($insert_data));
     $values = "'" . implode("', '", $escaped_values) . "'";
     return "INSERT INTO " . $entity . "_tags VALUES ($values);\n";
 }
 
 
-//function insert_entity_node($entity, $entity_id, $node_id, $sequence)
-//{
-//    $insert_data = array(
-//        $entity . "_id" => $entity_id,
-//        "node_id" => $node_id,
-//        "sequence" => $sequence
-//    );
-//
-//    $escaped_values = array_map('mb_escape', array_values($insert_data));
-//    $values = "'" . implode("', '", $escaped_values) . "'";
-//    return "INSERT INTO " . $entity . "_nodes VALUES ($values);\n";
-//
-//}
-//
-//function insert_entity_member($entity, $entity_id, $values, $sequence)
-//{
-//    $insert_data = array(
-//        $entity . "_id" => $entity_id,
-//        "member_type" => $values["type"],
-//        "member_id" => $values["ref"],
-//        "member_role" => $values["role"],
-//        "sequence" => $sequence
-//    );
-//
-//    $escaped_values = array_map('mb_escape', array_values($insert_data));
-//    $values = "'" . implode("', '", $escaped_values) . "'";
-//    return "INSERT INTO " . $entity . "_membars VALUES ($values);\n";
-//
-//}
+function insert_entity_node($entity, $entity_id, $node)
+{
+    $insert_data = array(
+        $entity . "_id" => $entity_id,
+        "node_id" => $node["id"],
+        "sequence" => $node["sequence"]
+    );
+
+    $escaped_values = array_map('escape', array_values($insert_data));
+    $values = "'" . implode("', '", $escaped_values) . "'";
+    return "INSERT INTO " . $entity . "_nodes VALUES ($values);\n";
+
+}
+
+function insert_entity_member($entity, $entity_id, $values)
+{
+    $insert_data = array(
+        $entity . "_id" => $entity_id,
+        "member_type" => $values["member_type"],
+        "member_id" => $values["member_id"],
+        "member_role" => $values["member_role"],
+        "sequence" => $values["sequence"]
+    );
+    $escaped_values = array_map('escape', array_values($insert_data));
+    $values = "'" . implode("', '", $escaped_values) . "'";
+    return "INSERT INTO " . $entity . "_members VALUES ($values);\n";
+
+}
+
+function escape($string)
+{
+    return preg_replace('~[\x00\x0A\x0D\x1A\x22\x27\x5C]~u', '\\\$0', $string);
+}
